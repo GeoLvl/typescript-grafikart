9807ea99f52bc504895c8df3d0a0a7ee12b706ac
NyanBot <laville.geoffrey@gmail.com>
2023-03-03 17:22:51 +0100

    Learn TS grafikart 1>4

---
+++ app.ts
@@ -0,0 +1,15 @@
+const compteur = document.querySelector('#compteur') as HTMLButtonElement
+
+let i = 0;
+
+const increment = (e: Event) => {
+    e.preventDefault();
+    i++;
+    const span = compteur?.querySelector('span')
+
+    if (span) {
+        span.innerText = i.toString()
+    }
+}
+
+compteur?.addEventListener('click', increment)  
